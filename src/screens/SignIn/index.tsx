bf3e8995ec359a8115507c4b9b19805bd28ca884
RennanD <rplaykl@gmail.com>
2022-07-18 20:11:21 -0300

    feat: create singIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+
 import { VStack, Heading } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -6,24 +10,42 @@ import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
 export function SignIn(): JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // function handleSingIn() {
+  //   console.log({ email, password });
+  // }
+
   return (
-    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-      <Logo />
-      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        {' '}
-        SignIn
-      </Heading>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+        <Logo />
+        <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+          {' '}
+          SignIn
+        </Heading>
 
-      <TextInput
-        placeholder="E-mail"
-        mb={4}
-        icon={Envelope}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
-      <TextInput mb={8} placeholder="Senha" icon={Key} secureTextEntry />
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          placeholder="E-mail"
+          mb={4}
+          icon={Envelope}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          mb={8}
+          placeholder="Senha"
+          icon={Key}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
 
-      <Button w="full">Entrar</Button>
-    </VStack>
+        <Button w="full">Entrar</Button>
+      </VStack>
+    </TouchableWithoutFeedback>
   );
 }
