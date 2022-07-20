f24f6d4102dfb9bc7d755d64663fd387203fc695
RennanD <rplaykl@gmail.com>
2022-07-20 10:45:46 -0300

    feat: pass params between routes

--- src/screens/OrderDetails/index.tsx
+++ src/screens/OrderDetails/index.tsx
@@ -1,10 +1,21 @@
-import { VStack } from 'native-base';
+import { useRoute } from '@react-navigation/native';
+import { Text, VStack } from 'native-base';
 import { Header } from '../../components/Header';
 
+type RouteParams = {
+  orderId: string;
+};
+
 export function OrderDetails(): JSX.Element {
+  const route = useRoute();
+
+  const { orderId } = route.params as RouteParams;
+
   return (
     <VStack flex={1} bg="gray.700">
       <Header title="Detalhes da Solicitação" />
+
+      <Text color="white">{orderId}</Text>
     </VStack>
   );
 }
