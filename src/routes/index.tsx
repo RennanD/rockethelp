24c35ddf884e7f36beb8ad6f4a190feb8a7fc50f
RennanD <rplaykl@gmail.com>
2022-07-21 15:33:21 -0300

    feat: sign in user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,36 @@
 import { NavigationContainer } from '@react-navigation/native';
 
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+import { useEffect, useState } from 'react';
 import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.routes';
+import { Loading } from '../components/Loading';
 
 // import { Container } from './styles';
 
+type UserData = FirebaseAuthTypes.User;
+
 export function Routes(): JSX.Element {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(response => {
+      setUser(response);
+      setLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </NavigationContainer>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 
-import { TouchableWithoutFeedback, Keyboard } from 'react-native';
-
+import { TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { VStack, Heading } from 'native-base';
+
 import { Envelope, Key } from 'phosphor-react-native';
+
+import auth from '@react-native-firebase/auth';
 
 import Logo from '../../assets/logo_primary.svg';
 import { Button } from '../../components/Button';
@@ -12,10 +14,37 @@ import { TextInput } from '../../components/TextInput';
 export function SignIn(): JSX.Element {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  // function handleSingIn() {
-  //   console.log({ email, password });
-  // }
+  function handleSingIn() {
+    if (!email || !password) {
+      Alert.alert('Erro', 'Preencha todos os campos');
+      return;
+    }
+
+    setLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        setLoading(false);
+
+        if (error.code === 'auth/invalid-email') {
+          Alert.alert('Erro', 'Digite um e-mail válido');
+          return;
+        }
+
+        if (
+          error.code === 'auth/user-not-found' ||
+          error.code === 'auth/wrong-password'
+        ) {
+          Alert.alert('Erro', 'Credenciais inválidas');
+          return;
+        }
+
+        Alert.alert('Erro', 'Não foi possível realizar o login');
+      });
+  }
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -23,7 +52,7 @@ export function SignIn(): JSX.Element {
         <Logo />
         <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
           {' '}
-          SignIn
+          Acessar sua conta
         </Heading>
 
         <TextInput
@@ -44,7 +73,9 @@ export function SignIn(): JSX.Element {
           secureTextEntry
         />
 
-        <Button w="full">Entrar</Button>
+        <Button isLoading={loading} onPress={() => handleSingIn()} w="full">
+          Entrar
+        </Button>
       </VStack>
     </TouchableWithoutFeedback>
   );
